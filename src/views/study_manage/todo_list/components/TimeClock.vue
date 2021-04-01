d695fe04c1957fe0a37715392d4513cce8293f1d
shaoning <904809622@qq.com>
2021-04-01 14:32:56 +0800

    update TimeClock CSS font and style

--- src/views/study_manage/todo_list/components/TimeClock.vue
+++ src/views/study_manage/todo_list/components/TimeClock.vue
@@ -14,6 +14,7 @@ export default {
     }
   },
   mounted() {
+    this.updateTime()
     setInterval(this.updateTime, 1000)
   },
   methods: {
@@ -38,18 +39,38 @@ export default {
 <style scoped>
 #clock {
   font-family: 'Share Tech Mono', monospace;
-  /*background: #686868;*/
-  background: radial-gradient(black, transparent);
+  /*font-family: 'Special Elite', monospace;*/
+  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+  background-size: 400% 400%;
   color: #daf6ff;
   text-align: center;
   text-shadow: 0 0 20px #0aafe6, 0 0 20px rgba(10, 175, 230, 0.1);
-  /*border: 0.5vw solid rgba(255, 255, 255, 0.3);*/
+  letter-spacing: 5px;
+  padding: 10px 0 10px 0;
+  animation: changeBackground 20s infinite linear alternate;
 }
-
+@keyframes changeBackground {
+	0% {background-position: 0 50%;}
+	50% {background-position: 100% 50%;}
+	100% {background-position: 0 50%;}
+}
+@font-face {
+    font-family: 'Special Elite';
+    font-style: normal;
+    font-weight: 400;
+    src: local("Special Elite Regular"),local("SpecialElite-Regular"),url('font_files/SpecialElite-webfont.woff.ttf') format("woff2");
+    unicode-range: U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD
+}
+@font-face {
+    font-family: 'Share Tech Mono';
+    font-style: normal;
+    font-weight: 400;
+    src: local("Special Elite Regular"),local("SpecialElite-Regular"),url('font_files/Share-Tech-Mono.ttf') format("woff2");
+    unicode-range: U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD
+}
 #clock .time {
-  /*letter-spacing: 0.05em;*/
-  /*font-size: 80px;*/
   font-size: 4vw;
+  font-weight: 900;
   padding: 5px 0;
 }
 </style>
